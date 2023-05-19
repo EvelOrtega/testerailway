763213603866ac367b764a6651a8165ee84c8f0f
Evel Ortega <102565878+EvelOrtega@users.noreply.github.com>
2023-05-19 20:12:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // function
-app.use("/", res.status(200).send("funcionou"));
+app.use("/", "funcionou"));
 
 
 // Listen on `port` and 0.0.0.0
